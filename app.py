03dd6e34497150709a1e8ae63cd439db9d6ada99
we-go-high <joehdoo65@gmail.com>
2021-12-23 22:31:30 +0900

    first commit

--- app.py
+++ app.py
@@ -5,11 +5,35 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
+from pymongo import MongoClient
+client = MongoClient('mongodb://test:test@3.34.1.132:27017/')
+# localgost는 대표 ip주소, 컴퓨터 포트27017
+db = client.data_db
+# collection에 연결
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
+# API 데이터불러오기 테스트
+@app.route('/test', methods=['GET'])
+def datatest_get():
+   articles = list(db.toursdata.find({}, {'_id': False}))
+   print(articles)
+   return jsonify({'articles': articles})
+
+
+# API POST 테스트
+@app.route('/test', methods=['POST'])
+def test_post():
+   thema_receive = request.form['thema_give']
+
+   doc = {
+       'thema': thema_receive
+   }
+   db.themes.insert_one(doc)
+
+   return jsonify({'result':'success', 'msg': '검색완료'})
 
 
 if __name__ == "__main__":
